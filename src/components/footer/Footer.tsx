aa3b944c73d35fe7a1ee73055a0955461013d6a7
Julia T <juliaturkali@gmail.com>
2025-03-05 14:24:27 +0100

    Refactor layout to conditionally render footer and enhance footer component with content and styling

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,19 @@
+import Logo from '../../assets/img/Logo.png';
+import FooterDesk from '../../assets/img/Footer_Desk.png';
+
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className="relative font-[Nunito] text-gray-800 bg-[#F1F1F1] flex justify-between items-center">
+      <article className="pl-20 border-t-1 border-gray-300 w-2/3 py-6">
+        <img src={Logo} alt="Woodies Logo" />
+        <p>(012) 8967453</p>
+        <p>woodies@gmail.com</p>
+        <p>Jakarta, Indonesia</p>
+      </article>
+      <p className="absolute bottom-6 left-1/2">© 2020 WOODIES</p>
+      <img src={FooterDesk} alt="Desk" />
+    </footer>
+  );
 };
 
 export default Footer;
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,15 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 
 const Layout = () => {
+  // useLocation() gibt uns den aktuellen Pfad zurück
+  const location = useLocation();
   return (
     <>
       <Header />
       <main>
         <Outlet />
       </main>
-      <Footer />
+      {/* Footer wird nur angezeigt, wenn nicht auf Home */}
+      {location.pathname !== '/' && <Footer />}
     </>
   );
 };
