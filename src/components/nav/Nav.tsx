746bc87b13909fed512e7250003064c89b248c9f
Julia T <juliaturkali@gmail.com>
2025-03-05 15:59:35 +0100

    Fix navigation width to be full-screen  to prevent horizontal scrolling

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -3,7 +3,7 @@ import Logo from '../../assets/img/Logo.png';
 
 const Nav = () => {
   return (
-    <nav className="absolute top-0 left-0 w-dvw flex justify-between items-center py-4 px-20 bg-transparent text-gray-800 ">
+    <nav className="absolute top-0 left-0 w-full flex justify-between items-center py-4 px-20 bg-transparent text-gray-800 ">
       <img src={Logo} alt="Woodies Logo" />
       <div className="flex gap-10">
         <Link to="/">Home</Link>
